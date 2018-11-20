e5ed2fe68223b3330855c3e859f6a4239cbf1bee
T.shiva sai <44740539+shivasai9@users.noreply.github.com>
2018-11-20 12:54:25 +0530

    Add files via upload

---
+++ week2/angularjs/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  st=[
+    {name:"shiva", rno:1, marks:100, depat:'ece'},
+    {name:"anvesh", rno:2, marks:90, depat:'cse'},
+    {name:"ravi", rno:3, marks:85, depat:'ece'},
+    {name:"Murali", rno:4, marks:100, depat:'cse'},
+    {name:"rohith", rno:5, marks:75, depat:'ece'},
+    {name:"Shiva sai", rno:6, marks:95, depat:'ece'},
+    {name:"Uthej", rno:7, marks:85, depat:'cse'}
+  ];
+
+}
