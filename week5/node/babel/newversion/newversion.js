206e8c88438c271b07c96d89ba80efd6a3fde427
T.shiva sai <44740539+shivasai9@users.noreply.github.com>
2018-12-19 22:46:39 +0530

    Add files via upload

---
+++ week5/node/babel/newversion/newversion.js
@@ -0,0 +1,10 @@
+var x=()=>{
+    var a=[1,2,3];
+    a.map(x=>x*2);
+
+    var s1="shiva";
+    var s2="sai";
+    console.log(`my name is ${s1} is ${s2} `);
+
+    var [a,,,,,b]=[1,2,3,4,5,6];
+}
---
+++ week5/node/babel/oldversion/newversion.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var x = function x() {
+    var a = [1, 2, 3];
+    a.map(function (x) {
+        return x * 2;
+    });
+
+    var s1 = "shiva";
+    var s2 = "sai";
+    console.log("my name is " + s1 + " is " + s2 + " ");
+
+    var _ref = [1, 2, 3, 4, 5, 6],
+        a = _ref[0],
+        b = _ref[5];
+};
